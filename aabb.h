d6704b739fcbf8d4ca4b1dd13ffbe4d44db1c690
Ahmet Inan <ainan@mathematik.uni-freiburg.de>
2013-03-25 10:29:24 +0100

    replaced v4sf l[2] with float l[2]

--- aabb.h
+++ aabb.h
@@ -16,12 +16,12 @@ struct aabb
 	v4sf c0, c1;
 };
 
-int aabb_ray(v4sf l[2], struct aabb box, struct ray ray)
+int aabb_ray(float l[2], struct aabb box, struct ray ray)
 {
 	v4sf a = (box.c0 - ray.o) * ray.inv_d;
 	v4sf b = (box.c1 - ray.o) * ray.inv_d;
-	l[0] = v4sf_hmax3(v4sf_min(a, b));
-	l[1] = v4sf_hmin3(v4sf_max(a, b));
-	return v4sf_lt1(l[0], l[1])[0];
+	l[0] = v4sf_hmax3_float(v4sf_min(a, b));
+	l[1] = v4sf_hmin3_float(v4sf_max(a, b));
+	return l[0] < l[1];
 }
 #endif
