ca34a9f077aca870941defdd01d8d7b6f5218353
Ahmet Inan <ainan@mathematik.uni-freiburg.de>
2013-07-16 22:23:51 +0200

    added i4sf_{set1,set,div,neg}

--- ia.h
+++ ia.h
@@ -15,12 +15,33 @@ typedef struct {
 	v4sf max;
 } i4sf;
 
+static inline i4sf i4sf_set1(float min, float max)
+{
+	return (i4sf){{ min, min, min, min }, { max, max, max, max }};
+}
+
+static inline i4sf i4sf_set(v4sf min, v4sf max)
+{
+	return (i4sf){ min, max };
+}
+
 static inline i4sf i4sf_mul(i4sf a, i4sf b)
 {
 	v4sf ab0 = a.min * b.min;
 	v4sf ab1 = a.min * b.max;
 	v4sf ab2 = a.max * b.min;
 	v4sf ab3 = a.max * b.max;
+	v4sf min = v4sf_min(v4sf_min(v4sf_min(ab0, ab1), ab2), ab3);
+	v4sf max = v4sf_max(v4sf_max(v4sf_max(ab0, ab1), ab2), ab3);
+	return (i4sf){ min, max };
+}
+
+static inline i4sf i4sf_div(i4sf a, i4sf b)
+{
+	v4sf ab0 = a.min / b.min;
+	v4sf ab1 = a.min / b.max;
+	v4sf ab2 = a.max / b.min;
+	v4sf ab3 = a.max / b.max;
 	v4sf min = v4sf_min(v4sf_min(v4sf_min(ab0, ab1), ab2), ab3);
 	v4sf max = v4sf_max(v4sf_max(v4sf_max(ab0, ab1), ab2), ab3);
 	return (i4sf){ min, max };
@@ -34,6 +55,11 @@ static inline i4sf i4sf_add(i4sf a, i4sf b)
 static inline i4sf i4sf_sub(i4sf a, i4sf b)
 {
 	return (i4sf){ a.min - b.max, a.max - b.min };
+}
+
+static inline i4sf i4sf_neg(i4sf a)
+{
+	return (i4sf){ -a.max, -a.min };
 }
 
 #endif
