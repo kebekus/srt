f7e4dec2c61c2e8f5ee4562bb91980db2972a047
Stefan Kebekus <stefan.kebekus@web.de>
2013-08-09 14:35:43 +0200

    I started to document the classes in libqsrt and to write unit tests for every feature that appears in the documentation.

---
+++ src/libqsrt/srtSurface_test.cpp
@@ -0,0 +1,22 @@
+#include <QtTest/QtTest>
+
+#include "srtSurface_test.h"
+#include "srtSurface.h"
+
+
+void srtSurface_test::constructor()
+{
+  QVERIFY( 1==1 );
+}
+
+
+void srtSurface_test::equality()
+{
+  QVERIFY( 1==1 );
+}
+
+
+void srtSurface_test::storage()
+{
+  QVERIFY( 1==1 );
+}
---
+++ src/libqsrt/srtSurface_test.h
@@ -0,0 +1,11 @@
+ #include <QtTest/QtTest>
+
+ class srtSurface_test : public QObject
+ {
+   Q_OBJECT
+     
+  private slots:
+   void constructor();
+   void equality();
+   void storage();
+ };
